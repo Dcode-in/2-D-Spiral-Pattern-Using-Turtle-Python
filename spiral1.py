87931dcf6e503fd4bad2ab03f80bf7b6724e5282
Dcode-in <75630464+Dcode-in@users.noreply.github.com>
2020-12-22 18:15:18 +0530

    Update spiral1.py

--- spiral1.py
+++ spiral1.py
@@ -10,11 +10,11 @@ x = 1
 while x < 400:
     
     colormode(255) # this is something that is irrelevant at this point
-               # check the pythondocs link at the end for more info
+              
 
 
     pencolor(255,255,255) # changes the color of the pen to the rgb coordinates
-                    # obtained by the variables r, g, b changing each time
+                    
 
     fd(50 + x)
     rt(90.911)
